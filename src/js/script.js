fc073fadc5983da52126143dfd54e5291933d73e
Evgeny Lebedevsky <115792378+Lightzone1981@users.noreply.github.com>
2022-11-30 00:22:11 +0300

    service slider custom styles added

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+new Swiper('.service-slider', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    slidesPreview: 2,
+    spaceBetween: 20,
+});
