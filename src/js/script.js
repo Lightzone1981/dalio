e0d0e8ac1927704bff28ef7925bad26c5a00dda9
Evgeny Lebedevsky <115792378+Lightzone1981@users.noreply.github.com>
2022-11-30 19:45:54 +0300

    projets article completed

--- src/js/script.js
+++ src/js/script.js
@@ -13,9 +13,14 @@ new Swiper('.projects-slider', {
         el: '.swiper-pagination',
         clickable: true,
     },
-    slidesPreview: 2,
+    slidesPerView: 1,
     spaceBetween: 20,
 }); 
+
+new Swiper('.projects__nav-slider', {
+    
+    slidesPerView: 3.2,
+});
 
 
 const mediaQuery = window.matchMedia('(max-width: 768px)')
